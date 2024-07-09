6c16438e99a8ff1a94ca27207c19268a18a96b0d
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-09 13:04:33 +0700

    test(Gameboard): write test for placeShip method

--- src/modules/tests/Gameboard.test.js
+++ src/modules/tests/Gameboard.test.js
@@ -1,5 +1,6 @@
 import Gameboard from "../classes/Gameboard";
 import Cell from "../classes/Cell";
+import Ship from "../classes/Ship";
 
 test("constructs playing field", () => {
   const gameboard = new Gameboard(3);
@@ -7,5 +8,19 @@ test("constructs playing field", () => {
     [new Cell(), new Cell(), new Cell()],
     [new Cell(), new Cell(), new Cell()],
     [new Cell(), new Cell(), new Cell()],
+  ]);
+});
+
+test("places ships", () => {
+  const gameboard = new Gameboard(4);
+  gameboard.placeShip(3, 0, 1, true);
+  gameboard.placeShip(2, 2, 0, false);
+  gameboard.placeShip(1, 3, 3, false);
+
+  expect(gameboard.playingField).toEqual([
+    [new Cell(), new Ship(3), new Ship(3), new Ship(3)],
+    [new Cell(), new Cell(), new Cell(), new Cell()],
+    [new Ship(2), new Cell(), new Cell(), new Cell()],
+    [new Ship(2), new Cell(), new Cell(), new Ship(1)],
   ]);
 });
