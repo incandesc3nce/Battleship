07a9068a173eddcbe1f9db4fdb88dab6765786e5
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-09 20:49:31 +0700

    feat: change argument from board selector to DOM board element

--- src/modules/components/fillBoard.js
+++ src/modules/components/fillBoard.js
@@ -1,8 +1,7 @@
-export default function fillBoard(selector) {
-  const board = document.querySelector(selector);
+export default function fillBoard(boardElement) {
   for (let i = 0; i < 100; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
-    board.appendChild(cell);
+    boardElement.appendChild(cell);
   }
 }
