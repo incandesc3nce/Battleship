b4b27a1bcf4a7314a07abcd2c375b5e2e3ce276f
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-09 22:48:20 +0700

    feat: add specific class to player cells so they could be selected

--- src/modules/components/fillPlayerBoard.js
+++ src/modules/components/fillPlayerBoard.js
@@ -4,6 +4,7 @@ export default function fillBoard(boardElement, gameboard) {
   for (let i = 0; i < 100; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
+    cell.classList.add("player-cell");
     boardElement.appendChild(cell);
   }
 }
