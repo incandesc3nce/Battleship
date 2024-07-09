fbe4a4207c19db146e090fa410baaa5ebff5c8aa
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-10 01:18:21 +0700

    feat: replace game start code with reset function

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
-import './style.css';
-import './index.html';
-import fillPlayerBoard from './modules/components/fillPlayerBoard.js';
-import fillOpponentBoard from './modules/components/fillOpponentBoard.js';
-import Gameboard from './modules/classes/Gameboard.js';
-import showPlayerShips from './modules/components/showPlayerShips.js';
+import "./style.css";
+import "./index.html";
+import reset from "./modules/game/reset.js";
 
-
-const board1 = document.querySelector('.board.board-left');
-const board2 = document.querySelector('.board.board-right');
-
-const gameboard1 = new Gameboard(10);
-const gameboard2 = new Gameboard(10);
-
-gameboard1.randomizeShips();
-gameboard2.randomizeShips();
-
-fillPlayerBoard(board1, gameboard1);
-fillOpponentBoard(board2, gameboard2, gameboard1);
-
-showPlayerShips(gameboard1);
+reset();
