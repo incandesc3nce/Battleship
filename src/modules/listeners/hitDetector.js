f66c178dc341a0edb55a9e0f11851c4bec1c9093
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-09 21:23:43 +0700

    feat: set different colors for miss and hit

--- src/modules/listeners/hitDetector.js
+++ src/modules/listeners/hitDetector.js
@@ -2,9 +2,12 @@ export default function hitDetector(cell, i, gameboard) {
   const row = Math.floor(i / 10);
   const col = i % 10;
   const madeHit = gameboard.receiveAttack(row, col);
+  const hitElement = document.createElement("div");
   if (madeHit) {
-    const hitElement = document.createElement("div");
     hitElement.classList.add("hit");
+    cell.appendChild(hitElement);
+  } else if (madeHit === false) {
+    hitElement.classList.add("miss");
     cell.appendChild(hitElement);
   }
 }
