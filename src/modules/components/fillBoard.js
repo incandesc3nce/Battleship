b55c84d6c62738b305c962fc3ec6f36fc0a81f03
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-09 21:46:03 +0700

    fix: prevent player from being able to hit their own board

--- src/modules/components/fillBoard.js
+++ src/modules/components/fillBoard.js
@@ -4,10 +4,10 @@ export default function fillBoard(boardElement, gameboard, isEnemy) {
   for (let i = 0; i < 100; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
-    isEnemy
-      ? cell.classList.add("enemy-cell")
-      : cell.classList.add("player-cell");
-    cell.addEventListener("click", hitDetector.bind(null, cell, i, gameboard));
+    if (isEnemy) {
+      cell.classList.add("enemy-cell") 
+      cell.addEventListener("click", hitDetector.bind(null, cell, i, gameboard));
+    } 
     boardElement.appendChild(cell);
   }
 }
